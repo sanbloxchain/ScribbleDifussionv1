8daf06c34a15aaf01296f0190389048140280b2a
massanto <ehrusan@gmail.com>
2023-02-28 17:58:10 +0700

    first commit

---
+++ components/footer.js
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+export default function Footer({ events }) {
+  return (
+    <footer className="w-full my-8">
+      <div className="text-center lil-text mt-8">
+        <div className="inline-block py-3 px-4 border bg-gray-200 rounded-lg">
+          Generate Your Image to Own NFT Art..
+        </div>
+      </div>
+
+    </footer>
+  );
+}
